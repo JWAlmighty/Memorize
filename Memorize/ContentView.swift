2f26f72da7d9fc6185cb2f8c96129a46f9b0784c
JWAlmighty <jwele2218@snu.ac.kr>
2024-02-03 17:05:28 +0900

    Required task 1 of assignment 1

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -11,32 +11,70 @@ import SwiftUI
 struct ContentView: View {
 //    let emojis: Array<String> = ["😘",  "🤣", "🙈", "👍", "👍"]
     let emojis = ["😘",  "🤣", "🙈", "👍", "👍"]
-
+    @State var cardCount: Int = 4
+    
     var body: some View {
-        HStack{
-            ForEach(emojis.indices, id: \.self){ index in
+        VStack{
+            ScrollView{
+                cards
+            }
+            Spacer()
+            cardCountAdjuster
+        }
+        .padding()
+    }
+    
+    var cards: some View{
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]){
+            ForEach(0..<cardCount, id: \.self){ index in
                 CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
             }
         }
         .foregroundColor(.orange)
-        .padding()
+        
+    }
+    var cardCountAdjuster: some View{
+        HStack{
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+    
+    func cardCountAdjust(by offset: Int, symbol: String) -> some View{
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset > emojis.count || cardCount + offset < 1)
+    }
+    
+    var cardRemover: some View{
+        cardCountAdjust(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+    }
+    var cardAdder: some View{
+        cardCountAdjust(by: 1, symbol: "rectangle.stack.badge.plus.fill")
     }
 }
+
 struct CardView: View{
     let content: String
     @State var isFaceUp: Bool = true
     var body: some View{
         ZStack(alignment: .center, content: {
             let base = RoundedRectangle(cornerRadius: 25.0)
-            if isFaceUp {
+            Group{
                 base.foregroundColor(.white)
                 base.strokeBorder(lineWidth: 1)
                 Text(content).font(.largeTitle)
             }
-            else{
-                RoundedRectangle(cornerRadius: 25)
-                    .foregroundColor(.orange)
-            }
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
+            
         })
         .onTapGesture(perform: {
 //            isFaceUp = !isFaceUp
