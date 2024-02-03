ad61e0cd620d48dfa02218f29ca3fa59b0441dad
JWAlmighty <jwele2218@snu.ac.kr>
2024-02-03 12:56:44 +0900

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Jewoo Lee on 2/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack{
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
+        }
+        .foregroundColor(.orange)
+        .padding()
+    }
+}
+
+struct CardView: View{
+    var isFaceUp: Bool = false
+    var body: some View{
+        ZStack{
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 25.0)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 25.0)
+                    .strokeBorder(lineWidth: 1)
+                Text("🐶").font(.largeTitle)
+            }
+            else{
+                RoundedRectangle(cornerRadius: 25)
+                    .foregroundColor(.orange)
+            }
+        }
+    }
+}
+
+
+
+
+#Preview {
+    ContentView()
+}
