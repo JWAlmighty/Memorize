420180cf7dbe19b89da08a2ceb79204c8b993d6f
JWAlmighty <jwele2218@snu.ac.kr>
2024-02-04 09:22:37 +0900

    Lecture 3

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -10,54 +10,24 @@ import SwiftUI
 // changes
 struct ContentView: View {
 //    let emojis: Array<String> = ["😘",  "🤣", "🙈", "👍", "👍"]
-    let emojis = ["😘",  "🤣", "🙈", "👍", "👍"]
-    @State var cardCount: Int = 4
+    let emojis = ["😘",  "🤣", "🙈", "👍", "👍", "🐶", "🦾", "🖕"]
     
     var body: some View {
-        VStack{
-            ScrollView{
-                cards
-            }
-            Spacer()
-            cardCountAdjuster
+        ScrollView{
+            cards
         }
         .padding()
     }
     
     var cards: some View{
         LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]){
-            ForEach(0..<cardCount, id: \.self){ index in
+            ForEach(emojis.indices, id: \.self){ index in
                 CardView(content: emojis[index])
                     .aspectRatio(2/3, contentMode: .fit)
             }
         }
         .foregroundColor(.orange)
         
-    }
-    var cardCountAdjuster: some View{
-        HStack{
-            cardRemover
-            Spacer()
-            cardAdder
-        }
-        .imageScale(.large)
-        .font(.largeTitle)
-    }
-    
-    func cardCountAdjust(by offset: Int, symbol: String) -> some View{
-        Button(action: {
-            cardCount += offset
-        }, label: {
-            Image(systemName: symbol)
-        })
-        .disabled(cardCount + offset > emojis.count || cardCount + offset < 1)
-    }
-    
-    var cardRemover: some View{
-        cardCountAdjust(by: -1, symbol: "rectangle.stack.badge.minus.fill")
-    }
-    var cardAdder: some View{
-        cardCountAdjust(by: 1, symbol: "rectangle.stack.badge.plus.fill")
     }
 }
 
---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,12 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Jewoo Lee on 2/4/24.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame{
+    var model: MemoryGame<String>
+}
