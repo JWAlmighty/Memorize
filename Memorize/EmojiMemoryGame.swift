d719300c19d5d31d63963d6b52848e009e257c46
JWAlmighty <jwele2218@snu.ac.kr>
2024-02-07 23:08:09 +0900

    lec 7

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -8,10 +8,12 @@
 import SwiftUI
 
 class EmojiMemoryGame: ObservableObject{
+    typealias Card = MemoryGame<String>.Card
+    
     private static let emojis = ["😘",  "🤣", "🙈", "👍", "🐶", "🦾",  "👆", "🖖", "🖐️"]
     
     private static func createMemoryGame() -> MemoryGame<String>{
-        return MemoryGame(numberOfPairsOfCards: 8){ pairIndex in
+        return MemoryGame(numberOfPairsOfCards: 2){ pairIndex in
             if emojis.indices.contains(pairIndex){
                 return emojis[pairIndex]
             }
@@ -23,7 +25,7 @@ class EmojiMemoryGame: ObservableObject{
         
     @Published private var model = createMemoryGame()
 
-    var cards: Array<MemoryGame<String>.Card>{
+    var cards: Array<Card>{
         return model.cards
     }
     
@@ -33,7 +35,7 @@ class EmojiMemoryGame: ObservableObject{
         objectWillChange.send()
     }
     
-    func choose(_ card: MemoryGame<String>.Card){
+    func choose(_ card: Card){
         model.choose(card)
     }
 }
--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -12,11 +12,13 @@ struct EmojiMemoryGameView: View {
     @ObservedObject var viewModel: EmojiMemoryGame
 //    let emojis: Array<String> = ["😘",  "🤣", "🙈", "👍", "👍"]
     private let aspectRatio: CGFloat = 2/3
+    private let spacing: CGFloat = 4 
 
     var body: some View {
         VStack{
             cards
                 .animation(.default, value: viewModel.cards)
+                .foregroundColor(.orange)
             Button("Shuffle"){
                 viewModel.shuffle()
             }
@@ -31,34 +33,6 @@ struct EmojiMemoryGameView: View {
                     viewModel.choose(card)
                 }
         }
-        .foregroundColor(.orange)
-        
-    }
-}
-
-struct CardView: View{
-    let card: MemoryGame<String>.Card
-    
-    init(_ card: MemoryGame<String>.Card) {
-        self.card = card
-    }
-    
-    var body: some View{
-        ZStack(alignment: .center, content: {
-            let base = RoundedRectangle(cornerRadius: 25.0)
-            Group{
-                base.foregroundColor(.white)
-                base.strokeBorder(lineWidth: 1)
-                Text(card.content)
-                    .font(.system(size: 200))
-                    .minimumScaleFactor(0.01)
-                    .aspectRatio(1, contentMode: .fit)
-            }
-            .opacity(card.isFaceUp ? 1 : 0)
-            base.fill().opacity(card.isFaceUp ? 0 : 1)
-            
-        })
-        .opacity(card.isFaceUp || !card.isMatched ? 1: 0)
         
     }
 }
